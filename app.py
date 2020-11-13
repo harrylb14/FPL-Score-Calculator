bec00628a65a15fd17c5c1d0ba959aa657c5f872
HarryLB <harry.lingardbright@gmail.com>
2020-11-13 22:36:10 +0000

    refactoring

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ def gameweek_scores():
         {'name': 'Harry', 'team_id': '278724'},
         {'name': 'Alex', 'team_id': '422587'}
     ]
-    scores = []
+    all_player_scores = []
     for player in players:
-        player_score = []
+        player_scores = []
         team_id = player['team_id']
         url = f'https://fantasy.premierleague.com/api/entry/{team_id}/history/'
         r = requests.get(url)
         json = r.json()
         data = json['current']
         for week in data:
-            player_score.append(week['points'] - week['event_transfers_cost'])
-        scores.append(player_score)
+            player_scores.append(week['points'] - week['event_transfers_cost'])
+        all_player_scores.append(player_scores)
 
-        print(scores)
+    print(all_player_scores)
 
-    return render_template('full_scores.html', records=scores, colnames=['GameWeek', 'JH Score', 'Haz Score', 'Alex Score'])
+    return render_template('full_scores.html', records=all_player_scores, colnames=['GameWeek', 'JH Score', 'Haz Score', 'Alex Score'])
 
 
 
